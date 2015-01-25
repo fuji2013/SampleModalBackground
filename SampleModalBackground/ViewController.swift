eb8bb49b6fc0ec61fddec3acb407a7f60a7104d0
fuji2013 <fuhi1983@gmail.com>
2015-01-25 23:32:09 +0900

    モーダル画面遷移で背景透過させる

--- SampleModalBackground/ViewController.swift
+++ SampleModalBackground/ViewController.swift
@@ -9,10 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let btn = UIButton.buttonWithType(.System) as UIButton
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        btn.frame = CGRectMake(0, 0, 100, 100)
+        btn.center = view.center
+        btn.setTitle("タップ", forState: .Normal)
+        btn.addTarget(self, action: "pressBtn:", forControlEvents: .TouchUpInside)
+        view.addSubview(btn)
+    }
+    
+    func pressBtn(sender:UIButton){
+        let controller = ModalViewController()
+        controller.modalPresentationStyle = UIModalPresentationStyle.OverCurrentContext
+        self.presentViewController(controller, animated: true, completion: nil)
     }
 
     override func didReceiveMemoryWarning() {
